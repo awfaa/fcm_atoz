0b9c390ebe7bad25194430c70b860844d2376baf
awfaa <awfa.a@live.iium.edu.my>
2024-01-17 14:50:33 +0800

    added notifications

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter FCM Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthService().handleAuth(),
+    );
+  }
+}
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  handleAuth() {
+    return StreamBuilder(
+      stream: _auth.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return ChatScreen();
+        } else {
+          return SignInScreen();
+        }
+      },
+    );
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print("Error signing in: $e");
+    }
+  }
+}
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String email = _emailController.text.trim();
+                String password = _passwordController.text.trim();
+                _authService.signInWithEmailAndPassword(email, password);
+              },
+              child: Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,15 +102,66 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  User? _user;
+  late String _currentUserId;
   TextEditingController _messageController = TextEditingController();
-  List<String> _messages = []; // For storing chat messages
+  List<String> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("onMessage: $message");
+      _handleIncomingMessage(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("onMessageOpenedApp: $message");
+      // Handle message when app is in the foreground
+      _handleIncomingMessage(message);
+    });
+
+    _firebaseMessaging.getToken().then((token) {
+      print("FCM Token: $token");
+      // Save the token or send it to your server
+    });
+
+    _user = _auth.currentUser;
+    _currentUserId = _user!.uid;
+  }
+
+  void _handleIncomingMessage(RemoteMessage message) {
+    print('Received message: ${message.data}');
+    _updateUI(message.data);
+    _saveMessageToFirestore(message.data);
+  }
+
+  void _updateUI(Map<String, dynamic> messageData) {
+    String messageText = messageData['text'];
+    setState(() {
+      _messages.add(messageText);
+    });
+  }
+
+  void _saveMessageToFirestore(Map<String, dynamic> messageData) {
+    _firestore.collection('messages').add({
+      'text': messageData['text'],
+      'senderId': messageData['senderId'],
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+  }
 
   void _sendMessage(String message) {
-    setState(() {
-      _messages.add(message);
-      _messageController.clear();
+    _firestore.collection('messages').add({
+      'text': message,
+      'senderId': _currentUserId,
+      'timestamp': FieldValue.serverTimestamp(),
     });
-    // You can implement the logic to send messages to Firebase or handle it as needed.
   }
 
   @override
